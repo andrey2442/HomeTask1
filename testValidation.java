fb8cc5ef64e26cedb288d2eb74ce4dfff0655d6f
andrey2442 <andrey.kushch25@gmail.com>
2016-11-02 03:07:19 +0200

    Add files via upload

---
+++ testValidation.java
@@ -0,0 +1,12 @@
+package validation;
+import java.util.Scanner; 
+
+public class testValidation { 
+	
+public static void main(String[] args) { 
+
+validation v = new validation(); 
+
+v.validation();
+} 
+}
